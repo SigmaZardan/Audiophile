eedf012e3a07af8e0cfc4c99b49051bf5a4816c3
Bibek Bhujel <biekbhujel077@gmail.com>
2022-12-06 19:47:31 +0545

    Songs list UI completed

---
+++ app/src/main/java/com/bibek/audiophile/musicListActivity/MusicListActivity.java
@@ -0,0 +1,16 @@
+package com.bibek.audiophile.musicListActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bibek.audiophile.R;
+
+public class MusicListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_list);
+    }
+}
